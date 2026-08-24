b8810b97d64adeae51dbeed226b86bd219b9969d
hardcoregamingsyle <hardcorgamingstyle@gmail.com>
2026-08-24 21:30:52 +0530

    Coder: Creating the missing Vue.js frontend infrastructure for MoE Ultra Engine. Starting with the main ent...

---
+++ ui/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <main class="container mx-auto p-4">
+      <RouterView />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
+</script>
+
+<style scoped>
+#app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
